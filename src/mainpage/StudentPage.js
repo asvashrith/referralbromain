d141a035beda7320db28dfc770a670b966b2224e
Sai Rasazna Ajerla <saiajerla@gmail.com>
2023-05-04 20:04:40 -0600

    Add background color

--- src/mainpage/StudentPage.js
+++ src/mainpage/StudentPage.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { firebase, auth, storage } from '../firebase';
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import JobCards from '../admin/Jobcards';
+import '../Css/studentPage.css'
 
 const StudentPage = () => {
 
   return (
-    <div>
-    <p>Job oppurtunities for students and early careers</p>
-    <JobCards/>
+    <div class='bodyContainer'>
+      <p>Job oppurtunities for students and early careers</p>
+      <JobCards/>
     </div>
   );
 };

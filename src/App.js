3c25cdac9e726105e064d8edf3a7015cb18cd956
Sai Rasazna Ajerla <saiajerla@gmail.com>
2023-05-06 12:31:51 -0600

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -3,18 +3,40 @@ import {auth} from './firebase';
 import {useAuthState} from 'react-firebase-hooks/auth';
 import Login from './authentication/login';
 import Mainpage from './authentication/main';
-
+import {CircularProgress} from '@mui/material';
+import "./App.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+	const [user, setUser] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
 
-const [user] = useAuthState(auth);
-
-// here it'll check whether user is logged in or not if not it'll show login so while checking it'll take a sec in that time we need to add loader
-
-return (
-	user ? <Mainpage/> : <Login/>
-);
-
-}
+	/**
+	 * Here we set up a listener for authentication state changes.
+	 * Whenever the auth state changes, we update the user state with 
+	 * the new user object (or null if the user logs out), and display 
+	 * the loading spinner for 2 secs */
+	useEffect(() => {
+		const authUnsubscribe = auth.onAuthStateChanged(user => {
+			setUser(user);
+			setIsLoading(true);
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 2000); 
+		});
+		
+		return authUnsubscribe;
+	}, []);
+  
+	if (isLoading) {
+		return (
+			<div className="App-Loader">
+				<CircularProgress />
+			</div>
+		);
+	}
+  
+	return user ? <Mainpage /> : <Login />;
+  }
 
 export default App;

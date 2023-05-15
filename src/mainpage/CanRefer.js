6fad966a7672f49ac140a822987aef19fa704b1b
Sai Rasazna Ajerla <saiajerla@gmail.com>
2023-05-15 17:01:33 -0600

    Redesign and add functionality to can refer page

--- src/mainpage/CanRefer.js
+++ src/mainpage/CanRefer.js
@@ -1,52 +1,124 @@
 import React, { Component,useState,useEffect  } from 'react';
 import { firebase, auth, storage } from '../firebase';
+import "../Css/needReferral.css"
 
 const CanRefer = () => {
-    const [workEmail, setworkEmail] = useState("");
-    const [currentCompany, setcurrentCompany] = useState("");
-    const [currentRole, setcurrentRole] = useState("");
-    const [currentLocation, setcurrentLocation] = useState("");
-    const [phone, setphone] = useState("");
+    const [workEmail, setWorkEmail] = useState("");
+    const [currentCompany, setCurrentCompany] = useState("");
+    const [jobDepartment, setJobDepartment] = useState("");
+    const [experienceRequired, setExperienceRequired] = useState("");
 
 	const [isLoading, setisLoading] = useState(true);
-	const [usersData, setusersData] = useState('');
-	const [resumeFile, setresumeFile] = useState('');
+	const [userData, setUserData] = useState('');
+	
+    const [showVerification, setShowVerification] = useState(false);
+    const [verificationCode, setVerificationCode] = useState('');
+    const [emailVerified, setEmailVerified] = useState(false);
+
     const db = firebase.firestore();
     const userUniqueId = auth.currentUser.uid;
 
-    const handleChange =(e)=>{
-        
-        setworkEmail(e.target.value);
+    const handleSubmit = () => {
+        pushUserDetails();
+    }
+
+    const pushUserDetails = async () => {
+        try {
+            db.collection("userData").doc(auth.currentUser.uid).update({
+                referrerEmail: workEmail,
+                referrerCurrentCompany: currentCompany,
+                referrerJobDepartment: jobDepartment,
+                referrerExperienceRequired: experienceRequired,
+            });
+            console.log("details updated succesfully")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // Check if email address is valid
+    // TODO(srivatsav): If yes, send an OTP to the users email
+    const handleSendOTP = (e) => {
         let index = workEmail.indexOf('@');
         let indexqi2 = workEmail.indexOf('.');
-        if(indexqi2 > 0){
-            setcurrentCompany(workEmail.substring(index+1,indexqi2));
+        if (indexqi2 > 0) {
+            setCurrentCompany(workEmail.substring(index+1,indexqi2));
+            setShowVerification(true);
         }
-
     }
-    const sendVerificationLink = ()=>{
-        
-       }
-        return (
-        
-            <div class='needReferralBox'>
-                <p>Verify with your work email so we can share resumes of users for referral interested in your company</p>
-                <label class='label'>Enter your work email</label>
-                <input type="Text"
-                    value = {workEmail}
-                    onChange  = {(e)=>{handleChange(e)}} />
-                    
 
-                <label class='label'>Company</label>
-                <input type = "Text"
-                    value = {currentCompany}
-                    />
+    // TODO(srivatsav): Verify if OTP is valid
+    const handleVerify = () => {
+        if (verificationCode === "1234") {
+            setShowVerification(false);
+            setEmailVerified(true);
+        }
+    }
 
-                <br></br>
-                
-                <button onClick={sendVerificationLink}>Submit</button>
-
+    return (
+        <section class="referral-form">
+            <div>
+                <p>
+                    Make referring talented candidates to your company easy. Simply share your work email to receive resumes directly in your inbox from potential candidates actively seeking work. Help us find top talent and earn a bonus by referring great candidates!
+                </p>
             </div>
+            <div class="referral-form__separator" />
+            <form class="referral-form__form">
+                {!showVerification ? (
+                    <>
+                        <label class="referral-form__label" htmlFor="work-email">
+                            Enter your work email:
+                        </label>
+                        <input class="referral-form__input" id="work-email" type="text" value={workEmail} onChange={(e) => setWorkEmail(e.target.value)} />
+
+                        {!emailVerified && (
+                            <div class="referral-form__submit-section">
+                                <button class="referral-form__submit-button" type="button" onClick={handleSendOTP}>
+                                    Send OTP
+                                </button>
+                            </div> 
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <label class="referral-form__label" htmlFor="verification-code">
+                            Enter the verification code:
+                        </label>
+                        <input class="referral-form__input" id="verification-code" type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
+                        <div class="referral-form__submit-section">
+                            <button class="referral-form__submit-button" type="button" onClick={handleVerify}>
+                                Verify email
+                            </button>
+                        </div>
+                    </>
+                )}
+                {emailVerified && (
+                    <>
+                        <label class="referral-form__label" htmlFor="current-company">
+                            Enter your current company:
+                        </label>
+                        <input class="referral-form__input" id="current-company" type="text" value={currentCompany} />
+
+                        <label class="referral-form__label" htmlFor="job-department">
+                            Enter the department for the open position:
+                        </label>
+                        <input class="referral-form__input" id="job-department" type="text" value={jobDepartment} placeholder="e.g. engineering, sales, marketing" onChange={(e) => setJobDepartment(e.target.value)} />
+
+                        <label class="referral-form__label" htmlFor="experience-required">
+                            Enter years of experience required:
+                        </label>
+                        <input class="referral-form__input" id="experience-required" type="number" value={experienceRequired} onChange={(e) => setExperienceRequired(e.target.value)} />
+
+                        <div class="referral-form__submit-section">
+                            <button class="referral-form__submit-button" type="button" onClick={handleSubmit}>
+                                Submit
+                            </button>
+                        </div>
+                    </>
+                )}
+
+            </form>
+        </section>
     );
 }
 
